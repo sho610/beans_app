fdc6e35467df30162c34400f615132ce4bcdffce
sho610 <s3uzuy@gmail.com>
2022-01-27 11:30:51 +0000

    マイグレイションコメントアウト

--- db/migrate/20220115065849_add_shop_id_to_prefectures.rb
+++ db/migrate/20220115065849_add_shop_id_to_prefectures.rb
@@ -1,5 +1,5 @@
 class AddShopIdToPrefectures < ActiveRecord::Migration[5.2]
   def change
-    add_column :prefectures, :shop_id, :integer
+    #add_column :prefectures, :shop_id, :integer
   end
 end
